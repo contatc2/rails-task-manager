36ddfdeb3fdf40b44762f2b8646fc35344a45837
clementine contat <clementine.contat@gmail.com>
2019-02-19 12:40:35 +0000

    created tasks actions

--- app/controllers/tasks_controller.rb
+++ app/controllers/tasks_controller.rb
@@ -1,2 +1,42 @@
 class TasksController < ApplicationController
+  before_action :set_task, only: %i[show edit update destroy]
+
+  def index
+    @tasks = Task.all
+  end
+
+  def show
+  end
+
+  def new
+    @task = Task.new
+  end
+
+  def create
+    Task.create(tasks_params)
+    redirect_to tasks_path
+  end
+
+  def edit
+  end
+
+  def update
+    @task.update(tasks_params)
+    redirect_to tasks_path
+  end
+
+  def destroy
+    @task.destroy
+    redirect_to tasks_path
+  end
+
+  private
+
+  def tasks_params
+    params.require(:task).permit(:title, :details, :completed)
+  end
+
+  def set_task
+    @task = Task.find(params[:id])
+  end
 end
